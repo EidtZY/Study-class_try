b02ed62948fa9ddcc60da8df52c2ec86dddddd6f
EidtZY <52123321+EidtZY@users.noreply.github.com>
2019-07-28 15:21:58 +0800

    \\\___\\\

---
+++ phase3/main.py
@@ -0,0 +1,12 @@
+import random
+
+from hero_class import Hero
+
+cjsh = Hero('苍狼末裔', '成吉思汗', '射手ARCHER')
+cjsh.show_presentation()
+
+pg = Hero('破晓之神', '盘古', '战士WORROR')
+pg.show_presentation()
+
+noboby = Hero('', '', '')
+noboby.show_presentation()
