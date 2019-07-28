8246e3832d92e3864f6bb37f9e77806244ea691d
EidtZY <52123321+EidtZY@users.noreply.github.com>
2019-07-28 17:53:09 +0800

    、、、、、、

---
+++ phase4/main.py
@@ -0,0 +1,10 @@
+from person_chinese import Person, Chinese
+
+print('-------cutting line-------')
+man = Person()
+man.talk()
+
+print('-------cutting line-------')
+CHinese_man = Chinese()
+CHinese_man.talk()
+CHinese_man.walk()
